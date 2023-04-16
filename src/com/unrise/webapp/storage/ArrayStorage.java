8ef7ee50cbf4e2a6a2228aafbc2b0318c9ee08c6
liliya <ismagilovaliliya95@gmail.com>
2023-04-16 16:41:43 +0300

    Lesson2Edit1

--- src/com/unrise/webapp/storage/ArrayStorage.java
+++ src/com/unrise/webapp/storage/ArrayStorage.java
@@ -2,49 +2,52 @@ package com.unrise.webapp.storage;
 
 import com.unrise.webapp.model.Resume;
 
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+
+    private static final int STORAGE_LIMIT = 10000;
+    private final Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size;
 
     public void clear() {
-        for (int i = 0; i < size; i++) {
+        Arrays.fill(storage,null);
+        size = 0;
+
+       /* for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
         size = 0;
+        */
     }
 
     public void update(Resume r){
         if (size > 0 && size < storage.length) {
             storage[size-1] = r;
-        }else{
-            System.out.println("Resume is empty");
         }
     }
 
     public void save(Resume r) {
         if (size < storage.length) {
-            System.out.println("Resume is not full");
             storage[size] = r;
             size++;
-        }else{
-            System.out.println("Resume is full");
         }
     }
 
     public Resume get(String uuid) {
-        if(size > 0){
-            System.out.println("Resume is not empty");
-            for (int i = 0; i < size; i++) {
-                if (storage[i].getUuid() == uuid) {
-                    return storage[i];
-                }
+        int index = 0;
+
+        if(size > 0) {
+            index = findIndex(uuid);
+            if (index > -1){
+                return storage[index];
+            }else{
+                System.out.println("Uuid: "+ uuid +" is not exist ");
             }
-        }else{
-            System.out.println("WARNING! Resume is empty");
         }
         return null;
     }
@@ -66,14 +69,17 @@ public class ArrayStorage {
             size--;
         }
     */
-        for (int i =0; i<size; i++){
-            if(uuid == storage[i].getUuid()){
+        int index = findIndex(uuid);
+
+        if (index > -1){
+            for (int i =index; i<size; i++){
                 storage[i] = storage[size-1];
                 storage[size-1] = null;
                 size--;
             }
+        }else{
+            System.out.println("Uuid: "+ uuid +" is not exist ");;
         }
-
     }
 
     /**
@@ -83,13 +89,25 @@ public class ArrayStorage {
         Resume[] resume = new Resume[size];
 
         for (int i = 0; i < size; i++) {
-            resume[i].equals(storage[i]);
+            resume[i] = storage[i];
         }
         return resume;
     }
 
     public int size() {
         return size;
+    }
+
+    public int findIndex(String uuid){
+
+        if(size > 0){
+            for (int i = 0; i < size; i++) {
+                if (storage[i].getUuid() == uuid) {
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 
 }
